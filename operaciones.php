46fca35c426a3eee70de8cc53f3141e5be0f940f
Esteban Vazquez| <86809885+EgVazquez@users.noreply.github.com>
2022-09-11 18:37:09 -0300

    Add files via upload
    Se realizaron los cambios a los archivos correspondientes

---
+++ operaciones.php
@@ -0,0 +1,37 @@
+<?php
+require_once 'CajaAhorro.php';
+require_once 'CuentaCorriente.php';
+
+
+session_start();
+$cuenta = unserialize($_SESSION['cuenta']);
+?>
+<!DOCTYPE html>
+<html lang="es-ar">
+<head>
+<meta charset="UTF-8">
+<title>Orientación a objetos</title>
+</head>
+<body>
+<h1>Home banking</h1>
+<p id="mensaje" style="color:red;">
+<?php if (isset($_GET['m'])) { echo $_GET['m'];} ?>
+</p>
+<form action="operacion.php" method="post">
+    <label for="saldo">Saldo de la cuenta: </label>
+    <input name="saldo" id="saldo" readonly value="<?php echo $_GET['s']  ?>"><br>
+    <label for="tipo">Tipo de operación: </label>
+    <select name="tipo">
+        <option value="e">Extracción</option>
+        <option value="d">Depósito</option>
+    </select><br>
+    <label for="monto">Monto: </label>
+    <input name="monto" type="number"><br>
+    <input type="submit" value="Realizar operación">
+</form>
+        <?php 
+            $cuenta->movimientos();
+        ?>
+</body>
+</html>
+
